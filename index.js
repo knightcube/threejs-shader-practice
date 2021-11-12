9358afb56040f908bfe5a0b6a172992a4246fa75
Rajat Kumar Gupta <rajat@revidd.com>
2021-11-12 07:12:24 +0530

    Added matrix code

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ import fShader from './shaders/fragmentShader.glsl.js'
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const customShaderMaterial = new THREE.ShaderMaterial({
-    vertexShader: vShader,
-    fragmentShader: fShader
-})
-const material = new THREE.MeshBasicMaterial({
-    color: 'blue'
-})
-const cubeMesh = new THREE.Mesh(geometry,customShaderMaterial)
-scene.add(cubeMesh)
-
 // Boilerplate code
 const sizes = {
     width: window.innerWidth,
@@ -26,6 +15,36 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
+
+const geometry = new THREE.BoxGeometry(2,2,2)
+//    uniform vec2 charSize;
+// uniform float charResolution;
+// uniform vec3 color;
+// uniform vec4 backgroundColor;
+// uniform vec2 resolution;
+const uniforms = {
+    time: {value:0.0},
+    u_resolution: { value:{ x:window.innerWidth, y:window.innerHeight }},
+    charSize:  { value:{ x:2.0, y:1.5 }},
+    charResolution: {value:5.5},
+    color: {value: new THREE.Color('green')},
+    resolution: { value:{ x:1.0, y:1.0 }},
+    speed: {value: 18.0}
+}
+
+const customShaderMaterial = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: vShader,
+    fragmentShader: fShader
+})
+console.log(uniforms)
+const material = new THREE.MeshBasicMaterial({
+    color: 'blue'
+})
+const cubeMesh = new THREE.Mesh(geometry,customShaderMaterial)
+scene.add(cubeMesh)
+
+
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableZoom = false;
@@ -38,11 +57,12 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
+const clock = new THREE.Clock()
 const tick = ()=>{
     renderer.render(scene,camera)
     window.requestAnimationFrame(tick)
     controls.update()
+    uniforms.time.value = clock.getElapsedTime()
 }
 
 tick()
--- shaders/vertexShader.glsl.js
+++ shaders/vertexShader.glsl.js
@@ -1,8 +1,42 @@
 const vertexShader = `
-void main(){
+    /**
+    * Example Vertex Shader
+    * Sets the position of the vertex by setting gl_Position
+    */
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-}
+    // Set the precision for data types used in this shader
+    precision highp float;
+    precision highp int;
+
+    // Default THREE.js uniforms available to both fragment and vertex shader
+    // uniform mat4 modelMatrix;
+    // uniform mat4 modelViewMatrix;
+    // uniform mat4 projectionMatrix;
+    // uniform mat4 viewMatrix;
+    // uniform mat3 normalMatrix;
+
+    // Default uniforms provided by ShaderFrog.
+    // uniform vec3 cameraPosition;
+    uniform float time;
+
+    // Default attributes provided by THREE.js. Attributes are only available in the
+    // vertex shader. You can pass them to the fragment shader using varyings
+    // attribute vec3 position;
+    // attribute vec3 normal;
+    // attribute vec2 uv;
+    attribute vec2 uv2;
+
+    varying vec2 vUv;
+
+    void main() {
+
+        vUv = uv;
+
+        // This sets the position of the vertex in 3d space. The correct math is
+        // provided below to take into account camera and object data.
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+
+    }
 `
 
 export default vertexShader
